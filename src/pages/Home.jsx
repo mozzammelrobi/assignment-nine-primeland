d508faa54dd7741e66a61eef1c1ccf95596e888c
mozzammelrobi <mozzammelfb@gmail.com>
2024-04-08 00:36:46 +0600

    slider add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,50 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+
 
 const Home = () => {
     return (
         <div>
-            <h1 className="text-3xl">this is home page</h1>
+
+            <Swiper
+             spaceBetween={30}
+             centeredSlides={true}
+             loop={true}
+             autoplay={{
+               delay: 2500,
+               disableOnInteraction: false,
+             }}
+             pagination={{
+               clickable: true,
+             }}
+             navigation={true}
+             modules={[Autoplay, Pagination, Navigation]}
+             className="mySwiper"
+        
+             >
+                <SwiperSlide>
+                    <div>
+                        <img className='w-full h-96' src="https://i.ibb.co/xDDLNQq/download.jpg" alt="" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div>
+                    <img className='w-full h-96' src="https://i.ibb.co/cDVSJDx/Real-Estate-Agent-Business-India.jpg" alt="" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div>
+                        <img className='w-full h-96' src="https://i.ibb.co/xDDLNQq/download.jpg" alt="" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div>
+                    <img className='w-full h-96' src="https://i.ibb.co/cDVSJDx/Real-Estate-Agent-Business-India.jpg" alt="" />
+                    </div>
+                </SwiperSlide>
+
+            </Swiper>
         </div>
     );
 };
